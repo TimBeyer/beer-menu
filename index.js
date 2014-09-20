ec42d8d77362277798752bbc952417b9c496e326
Tim Beyer <tim.beyer@gmail.com>
2014-09-20 15:58:57 +0200

    cycle code refactor

--- index.js
+++ index.js
@@ -1,39 +1,22 @@
 var items = document.querySelectorAll('.item');
+var descriptions = document.querySelectorAll('.description');
+var images = document.querySelectorAll('.image');
+
+var numSlides = items.length;
 
 var i = 0;
 setInterval(function () {
-  var current = i % items.length;
-  var previous = (i - 1) % items.length;
-  items[current].classList.toggle('active');
-  if (i >= 1) {
-    items[previous].classList.toggle('active');   
-  }
+  var current = i % numSlides;
+  var previous = (i - 1) % numSlides;
+
+  [items, descriptions, images].forEach(function (nodeList) {
+      nodeList[current].classList.toggle('active');
+      if (i >= 1) {
+        nodeList[previous].classList.toggle('active');   
+      }
+    
+  });
+
   i++;
-}, 5000);
-
-var descriptions = document.querySelectorAll('.description');
-
-var j = 0;
-setInterval(function () {
-  var current = j % descriptions.length;
-  var previous = (j - 1) % descriptions.length;
-  descriptions[current].classList.toggle('active');
-  if (j >= 1) {
-    descriptions[previous].classList.toggle('active');   
-  }
-  j++;
-}, 5000);
-
-var images = document.querySelectorAll('.image');
-
-var k = 0;
-setInterval(function () {
-  var current = k % images.length;
-  var previous = (k - 1) % images.length;
-  images[current].classList.toggle('active');
-  if (k >= 1) {
-    images[previous].classList.toggle('active');   
-  }
-  k++;
 }, 5000);
 
