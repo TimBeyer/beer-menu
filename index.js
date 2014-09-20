d4a7d72020c9d2ddd37286ba9cbaba0bacf8da16
Tim Beyer <tim.beyer@gmail.com>
2014-09-20 15:06:12 +0200

    pretty much everything so far

---
+++ index.js
@@ -0,0 +1,26 @@
+// var items = document.querySelectorAll('.item');
+
+// var i = 0;
+// setInterval(function () {
+//   var current = i % items.length;
+//   var previous = (i - 1) % items.length;
+//   items[current].classList.toggle('active');
+//   if (i >= 1) {
+//     items[previous].classList.toggle('active');   
+//   }
+//   i++;
+// }, 5000);
+
+// var descriptions = document.querySelectorAll('.description');
+
+// var j = 0;
+// setInterval(function () {
+//   var current = j % descriptions.length;
+//   var previous = (j - 1) % descriptions.length;
+//   descriptions[current].classList.toggle('active');
+//   if (j >= 1) {
+//     descriptions[previous].classList.toggle('active');   
+//   }
+//   j++;
+// }, 5000);
+
